4704803edf7f0ce4e548a91218d2f7effbf2f51c
rJinksFHS <jinksran20@gmail.com>
2025-04-23 13:12:33 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -87,18 +87,18 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            else if ( circle.x < 0 ) {
+            else if (circle.x < 0) {
                 circle.x = canvas.width;
             }
-            if ( circle.y > canvas.height ) {
+            if (circle.y > canvas.height) {
                 circle.y = 0;
             }
-            else if ( circle.y < 0 ) {
+            else if (circle.y < 0) {
                 circle.y = canvas.height;
             }
 
